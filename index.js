ecf4f94dafe06d775c4c61a896954c71bf2f481f
Mrinal-KANTI55 <baust.cse.170101055@gmail.com>
2021-11-10 20:17:13 +0600

    connect on mongodb

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000;
 
 app.use(cors())
 app.use(express.json());
+
+
+const { MongoClient } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.8iwul.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("Light-house").collection("User");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 
 app.get('/', (req, res) => {
